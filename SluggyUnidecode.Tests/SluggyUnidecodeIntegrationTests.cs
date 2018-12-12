50487e7a1a70342149ed7c2fdf6d43f649f66604
Ruben Mateus <ruben.mateus3@gmail.com>
2018-12-12 23:36:44 +0000

    upgraded version and small fix to unidecode

--- SluggyUnidecode.Tests/SluggyUnidecodeIntegrationTests.cs
+++ SluggyUnidecode.Tests/SluggyUnidecodeIntegrationTests.cs
@@ -11,6 +11,7 @@ namespace SluggyUnidecode.Tests
         [InlineData("ä ö ű ő Ä Ö Ũ Ő", "ae-oe-u-o-ae-oe-u-o")]
         [InlineData("eu não gosto de pão da avó", "eu-nao-gosto-de-pao-da-avo")]
         [InlineData("Работа с кириллицей", "rabota-s-kirillitsey")]
+        [InlineData("ch\u00e2teau vi\u00f1edos", "chateau-vinedos")]
         public void ShouldConvertToSlugWithUnidecode(string value, string expectation)
         {
             var slugified = value.ToSlug();
