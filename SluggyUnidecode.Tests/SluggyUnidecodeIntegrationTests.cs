273c8aebf6a3f82946528a5954a1f6d1b15885f9
Ruben Mateus <ruben.mateus3@gmail.com>
2018-12-13 23:31:57 +0000

    added support for sluggy 2.0.0, added testsfor the unidecodestrategy

--- SluggyUnidecode.Tests/SluggyUnidecodeIntegrationTests.cs
+++ SluggyUnidecode.Tests/SluggyUnidecodeIntegrationTests.cs
@@ -7,11 +7,13 @@ namespace SluggyUnidecode.Tests
         [Trait("Project", "SluggyUnidecode")]
         [Theory(DisplayName = "Should Convert ToSlug With Unidecode")]
         [InlineData("ČŽŠ čžš", "czs-czs")]
-        [InlineData("ア α a", "a-a-a")]
+        [InlineData("ア α a A", "a-a-a-a")]
+        [InlineData("Hello, World!", "hello,-world!")]
         [InlineData("ä ö ű ő Ä Ö Ũ Ő", "ae-oe-u-o-ae-oe-u-o")]
-        [InlineData("eu não gosto de pão da avó", "eu-nao-gosto-de-pao-da-avo")]
+        [InlineData("eu Não gosto de Pão da avó", "eu-nao-gosto-de-pao-da-avo")]
         [InlineData("Работа с кириллицей", "rabota-s-kirillitsey")]
-        [InlineData("ch\u00e2teau vi\u00f1edos", "chateau-vinedos")]
+        [InlineData("ch\u00e2teau Vi\u00f1edos", "chateau-vinedos")]
+        [InlineData("", "")]
         public void ShouldConvertToSlugWithUnidecode(string value, string expectation)
         {
             var slugified = value.ToSlug();
