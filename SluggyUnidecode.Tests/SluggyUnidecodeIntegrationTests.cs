9afab0ec06bb7f7a6618fa8a40278191a2105511
Ruben Mateus <ruben.mateus@hexis-hub.com>
2020-05-27 00:10:25 +0100

    feat: updated docs and metadata

--- SluggyUnidecode.Tests/SluggyUnidecodeIntegrationTests.cs
+++ SluggyUnidecode.Tests/SluggyUnidecodeIntegrationTests.cs
@@ -27,5 +27,29 @@ namespace SluggyUnidecode.Tests
 
             Assert.Equal(expectation, slugified);
         }
+
+        [Trait("Project", "SluggyUnidecode")]
+        [Theory(DisplayName = "Should Convert ToSlug With Unidecode and Separator")]
+        [InlineData("ČŽŠ čžš", "czs_czs", "_")]
+        [InlineData("ア α a A", "a_a_a_a", "_")]
+        [InlineData("Hello, World!", "hello_world", "_")]
+        [InlineData("ä ö ű ő Ä Ö Ũ Ő", "a_o_u_o_a_o_u_o", "_")]
+        [InlineData("eu Não gosto de Pão da avó", "eu_nao_gosto_de_pao_da_avo", "_")]
+        [InlineData("Работа с кириллицей", "rabota_s_kirillitsei", "_")]
+        [InlineData("Мне нравится татарин", "mne_nravitsia_tatarin", "_")]
+        [InlineData("ch\u00e2teau Vi\u00f1edos", "chateau_vinedos", "_")]
+        [InlineData("FAZLA ÇİKOLATA YEMEK DİŞ SAĞLIĞINI BOZABİLİR", "fazla_cikolata_yemek_dis_sagligini_bozabilir", "_")]
+        [InlineData("Fazla çikolata yemek diş sağlığını bozabilir", "fazla_cikolata_yemek_dis_sagligini_bozabilir", "_")]
+        [InlineData("PİJAMALI HASTA YAĞIZ ŞOFÖRE ÇABUCAK GÜVENDİ", "pijamali_hasta_yagiz_sofore_cabucak_guvendi", "_")]
+        [InlineData("pijamalı hasta yağız şoföre çabucak güvendi", "pijamali_hasta_yagiz_sofore_cabucak_guvendi", "_")]
+        [InlineData("1.10 もひとつ の せかい え", "110_mohitotsu_no_sekai_e", "_")]
+        [InlineData("a    <>*.,;´`'~^!#%$&/    n     ()=}{[]@£€§¨+|  ºª    a", "a_n_oa_a", "_")]
+        [InlineData("", "", "_")]
+        public void ShouldConvertToSlugWithUnidecodeWithSeparator(string value, string expectation, string separator)
+        {
+            var slugified = value.ToSlug(separator);
+
+            Assert.Equal(expectation, slugified);
+        }
     }
 }
